a17d16718b49650dfaaa202731a462c995734ec7
Erdem Bircan <erdem.erdembircan@gmail.com>
2019-03-20 00:34:46 +0300

    v1.0.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,6 +9,12 @@ if [[ $res =~ ^[yY]$ ]]
 then
   npm test
   npm run build
+
+  git add -A
+  git commit -m "v$version"
+  git tag -a v$version -m "v$version"
+  git push origin master --tags
+
   npm publish
 else
   echo "operation canceled"
